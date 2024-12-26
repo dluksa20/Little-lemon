dea75dc019a6efb32b88f1c92453331fbd06a99c
dluksa20 <dluksa20@gmail.com>
2024-12-26 02:49:51 +0000

    first-commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Hero.css';
-import slide1 from '../../assets/images/slider-images/slide1.jpg';
+import slide1 from '../../assets/images/slider-images/slide4.jpg';
 import logo from '../../assets/images/logos/logo.svg';
+import { FaArrowRight } from 'react-icons/fa';
 
 
 
@@ -11,12 +12,26 @@ const Hero = () => {
 
   return (
     <main>
-        
-        <div className='hero-wrapper' style={heroStyle}>
-            <div className='hero-content'>
-                <img src={logo} alt='logo' className='hero-logo' />
-                <h1 className='hero-title'>Welcome to<br />The Coffee House</h1>
-                <p className='hero-subtitle'>Enjoy the best coffee in town</p>
+      <div className='hero-logo-wrapper'>
+        <div className='hero-logo-container'>
+          <img src={logo} alt='logo' className='hero-logo' />
+        </div>
+        <div className='hero-button'>
+          <button>Order Online</button>
+          <button>Reserve a Table</button>
+        </div>
+      </div>
+        <div className='hero-text-wrapper' style={heroStyle}>
+          <div className='hero-overlay'></div>
+            <div className='hero-text-content'>
+                <h1 className='hero-title'>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <br />
+                <p className='hero-subtitle'>Authentic Mediterranean Flavors with a Modern Twist</p>
+                {/* Explore the Menu Button */}
+                <button className='explore-menu-button'>
+                  Explore the Menu <FaArrowRight />
+                </button>
             </div>
         </div>
     </main>
@@ -30,10 +45,10 @@ const heroStyle = {
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
-    height: '95vh',
+    height: '85vh',
+    width: '100%',
     position: 'relative', 
     borderRadius: '1.5rem',
-    margin: '1.5rem',
   };
 
 export default Hero;
