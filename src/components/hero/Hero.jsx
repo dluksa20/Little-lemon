4854d3ccc60d5042c95b3d0b456c2d45b909a8b4
dluksa20 <dluksa20@gmail.com>
2024-12-30 23:35:46 +0000

    slider-arrows/image compression

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import './Hero.css';
-import slide1 from '../../assets/images/slider-images/slide1.jpg';
-import slide2 from '../../assets/images/slider-images/slide2.jpg';
-import slide3 from '../../assets/images/slider-images/slide3.jpg';
-import slide4 from '../../assets/images/slider-images/slide4.jpg';
+import slide1 from '../../assets/images/slides/slide1.jpg';
+import slide2 from '../../assets/images/slides/slide2.jpg';
+import slide3 from '../../assets/images/slides/slide3.jpg';
+import slide4 from '../../assets/images/slides/slide4.jpg';
 import logo from '../../assets/images/logos/Logo.svg';
 import { FaArrowRight } from 'react-icons/fa';
 import ImageSlider from './ImageSlider';
 
-const slides = [
-  { url: slide1 },
-  { url: slide2 },
-  { url: slide3 },
-  { url: slide4 },
-];
+const slides = [ slide1, slide2, slide3, slide4];
 
 const Hero = () => {
   return (
@@ -27,18 +22,18 @@ const Hero = () => {
           <button aria-label="Reserve a Table">Reserve a Table</button>
         </div>
       </div>
-      <ImageSlider className='hero-slider' slides={slides}>
+      <ImageSlider className='hero-slider'  slides={slides}>
+        <div className='slider-content'>
+          <h1 className='hero-title'>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <br />
+          <p className='hero-subtitle'>Authentic Mediterranean Flavors with a Modern Twist</p>
+          {/* Explore the Menu Button */}
+          <button className='explore-menu-button'>
+            Explore the Menu <FaArrowRight />
+          </button>
+        </div>
 
-            <div className='slider-content'>
-              <h1 className='hero-title'>Little Lemon</h1>
-              <h2>Chicago</h2>
-              <br />
-              <p className='hero-subtitle'>Authentic Mediterranean Flavors with a Modern Twist</p>
-              {/* Explore the Menu Button */}
-              <button className='explore-menu-button'>
-                Explore the Menu <FaArrowRight />
-              </button>
-            </div>
       </ImageSlider>
     </main>
   );
--- src/components/hero/ImageSlider.jsx
+++ src/components/hero/ImageSlider.jsx
@@ -1,21 +1,63 @@
-import {React, useState} from 'react'
-import './ImageSlider.css'
+import React, { useState, useMemo, Children } from 'react';
+import './ImageSlider.css';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-const ImageSlider = ({slides, children,style, className}) => {
+const ImageSlider = ({ slides, children,style, className}) => {
+    const [current, setCurrent] = useState(0);
 
-    const [current, setCurrent] = useState(3)
+    const nextSlide = () => {
+        setCurrent((index) => (index === slides.length - 1 ? 0 : index + 1));
+    };
 
-  return (
-    <div className={className} style={{...style}}>
-        <div className='slider-image' style={{backgroundImage: `url(${slides[current].url})`}}>
+    const prevSlide = () => {
+        setCurrent((index) => (index === 0 ? slides.length - 1 : index - 1));
+    };
+
+
+    return (
+        <div className={className} style={{...style}}>
+            <div className="slider-wrapper">
+                {slides.map(slide => (
+                    <img
+                        key={slide}
+                        src={slide}
+                        alt={`slide-${slide}`}
+                        className="slider-img"
+                        loading='lazy'
+                        style={{ translate: `${-100 * current}%` }}
+                    />
+                ))}
+            </div>
             <div className='slider-overlay'>
                 <div className='slider-content-container'>
                     {children}
                 </div>
             </div>
-        </div>
-    </div>
-  )
-}
 
-export default ImageSlider
+            <button
+                className="slider-btn"
+                onClick={prevSlide}
+                style={{ left: 0, borderRadius: '1.5rem 0 0 1.5rem' }}
+            >
+                <MdChevronLeft />
+            </button>
+            <button
+                className="slider-btn"
+                onClick={nextSlide}
+                style={{ right: 0, borderRadius: '0 1.5rem 1.5rem 0' }}
+            >
+                <MdChevronRight />
+            </button>
+
+            <div className='slider-dots'>
+                {slides.map((_, index) => (
+                    <div 
+                    className={index === current ? 'active' : ''}
+                    onClick={() => setCurrent(index)}></div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ImageSlider;
