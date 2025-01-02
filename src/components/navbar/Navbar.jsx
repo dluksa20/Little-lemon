4c962ada904c6febc87570f98f34dea43f9b5bb9
dluksa20 <dluksa20@gmail.com>
2025-01-02 16:37:38 +0000

    new design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,87 +1,64 @@
-import React, { useState } from 'react'
+import React from 'react'
+import './Navbar.css'
 import { GoHome } from 'react-icons/go'
 import { GiKnifeFork } from 'react-icons/gi'
-import { MdOutlineTab } from 'react-icons/md'
-import { MdOutlineShoppingCart } from 'react-icons/md'
-import { IoMoonOutline } from 'react-icons/io5'
-import { CgProfile } from 'react-icons/cg'
-import './Navbar.css'
-import { Link, useLocation } from 'react-router-dom'
+import { MdOutlineTableRestaurant } from 'react-icons/md'
+import { MdOutlineShoppingCart } from 'react-icons/md'  
+import { NavLink } from 'react-router-dom'
+
+const navLinks = [
+{
+    title: 'Home',
+    icon: <GoHome />,
+    url: '/'
+},
+{
+    title: 'Menu',
+    icon: <GiKnifeFork />,
+    url: '/menu'
+},
+{
+    title: 'Book',
+    icon: <MdOutlineTableRestaurant />,
+    url: '/book'
+},
+{
+    title: 'Order',
+    icon: <MdOutlineShoppingCart />,
+    url: '/order'
+}
+];
 
 const Navbar = () => {
-    const [activeLink, setActiveLink] = useState('/');
-    const location = useLocation();
 
-    const navIcons = [
-        {
-            icon: <IoMoonOutline />,
-            link: '/theme',
-        },
-        {
-            icon: <CgProfile />,
-            title: 'Profile',
-            link: '/profile',
-        }
-    ];
 
-    const navLinks = [
-        {
-            title: 'Home',
-            icon: <GoHome />,
-            url: '/'
-        },
-        {
-            title: 'Menu',
-            icon: <GiKnifeFork />,
-            url: '/menu'
-        },
-        {
-            title: 'Book',
-            icon: <MdOutlineTab />,
-            url: '/book'
-        },
-        {
-            title: 'Order',
-            icon: <MdOutlineShoppingCart />,
-            url: '/order'
-        }
-    ];
-
-    const handleLinkClick = (url) => {
-        setActiveLink(url);
-    }
-
-    return (
-        <nav className='nav-wrapper'>
-            <div className='nav-links'>
-                {navLinks.map((link, index) => (
-                    <Link
-                        key={index}
-                        to={link.url}
-                        className={`nav-link ${location.pathname === link.url || activeLink === link.url ? 'active' : ''}`}
-                        onClick={() => handleLinkClick(link.url)}
-                    >
-                        {link.icon}
-                        <span>{link.title}</span>
-                    </Link>
-                ))}
+  return (
+    <nav className='nav-wrapper'>
+        <div className="nav-links">
+            {
+                navLinks.map((link, index) => (
+                <NavLink 
+                    key={index} 
+                    to={link.url}>
+                    {({ isActive }) => (
+                    <span className={`nav-item ${isActive ? "active" : ""}`}>
+                        {link.icon} {link.title}
+                    </span>
+                    )}
+                </NavLink>
+                ))
+            }
             </div>
 
-            <div className='nav-icons'>
-                {navIcons.map((icon, index) => (
-                    <Link
-                        key={index}
-                        to={icon.link}
-                        className={`nav-icon ${location.pathname === icon.link || activeLink === icon.link ? 'active' : ''}`}
-                        onClick={() => handleLinkClick(icon.link)}
-                    >
-                        {icon.icon}
-                        <span>{icon.title}</span>
-                    </Link>
-                ))}
-            </div>
-        </nav>
-    );
+        <div className='mode-toggler'>
+
+        </div>
+        <div className='profile-icon'>
+
+        </div>
+
+    </nav>
+  )
 }
 
-export default Navbar;
+export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import Hero from '../components/hero/Hero'
 
 const Home = () => {
   return (
-    <>
-      <Hero />
-    </>
+    <div>Home</div>
   )
 }
 
