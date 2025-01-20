7612154a2a7654d14d936a703350d80a050ef8f7
dluksa20 <dluksa20@gmail.com>
2025-01-20 17:54:45 +0000

    hero content

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,31 +3,40 @@ import './Hero.css';
 import ImageSlider from './ImageSlider.jsx';
 import { SliderData } from './SliderData';
 import { RiMenu3Fill, RiCloseFill, RiTwitterXFill, RiFacebookBoxFill, RiInstagramLine  } from "react-icons/ri";
-
+import Logo from '../../assets/images/logos/logo4.png';
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className='hero-wrapper'>
+      <div className="hero-slider">
+          <ImageSlider className="hero-slider" autoPlay={true} slides={SliderData} />
+        </div>
         <div className="hero-content">
+
           <div className="hero-title">
-            <h1>Little Lemon</h1>
+            <div className='hero-cta'>
+              <div className='logo-img'>
+                <img src={Logo} alt="" />
+              </div>
+              <div className="hero-description">
+                <p>
+                  Discover the finest cuisines, fresh ingredients, and a cozy ambiance. At Little Lemon, every dish is made with love and perfection.
+                </p>
+                <div className="hero-btn">
+                  <button>Reserve a Table</button>
+                  <button>Order Online</button>
+                </div>
+              </div>
+            </div>
+            
           </div>
-          <div className="hero-cta">
-            <p className="hero-description">
-              Discover the finest cuisines, fresh ingredients, and a cozy ambiance. At Little Lemon, every dish is made with love and perfection.
-            </p>
-            <button>Reserve a Table</button>
-            <button>Order Online</button>
-          </div>
+
           <div className='hero-socials'>
               <RiFacebookBoxFill />
               <RiInstagramLine />
               <RiTwitterXFill />
           </div>
-        </div>
-        <div className="hero-slider">
-          <ImageSlider className="hero-slider" autoPlay={true} slides={SliderData} />
         </div>
       </div>
       </div>
