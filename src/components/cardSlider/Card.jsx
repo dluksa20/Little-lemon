b26a80b39ec39edd9b37f4deeeb9fbfe6b1aabd1
dluksa20 <dluksa20@gmail.com>
2025-02-08 15:06:06 +0000

    card slider

--- src/components/cardSlider/Card.jsx
+++ src/components/cardSlider/Card.jsx
@@ -1,34 +1,29 @@
 import React from 'react'
+import { CiHeart } from "react-icons/ci";
+import { MdOutlineShoppingCart } from 'react-icons/md';
+
+
 
 export default function Card(props) {
     let bg_img = `url(${props.images})`
-    let { title, old_price, newPrice, dollar, exp_date } = props
+    let { title, item_price, currency} = props
     return (
 
-        <div className="card">
+        <div className="card">  
             <div className="wrapper">
-                <div className="card_img" style={{ "backgroundImage": bg_img }}></div>
+                <div className="card-img" style={{ "backgroundImage": bg_img }}/>
                 <div className="heart">
-                    <svg xmlns="<http://www.w3.org/2000/svg>" viewBox="0 0 64 64">
-                        <path d="M47 5c-6.5 0-12.9 4.2-15 10-2.1-5.8-8.5-10-15-10A15 15 0 0 0 2 20c0 13 11 26 30 39 19-13 30-26 30-39A15 15 0 0 0 47 5z">
-                        </path>
-                    </svg> 
+                    <CiHeart /> 
                 </div>
-                <div className="cardInfo">
+                <div className="card-info">
                     <h1>{title}</h1>
-                    <p className="date_">{exp_date}</p>
                     <div className="action">
                         <div className="priceGroup">
-                            <p className="price old_price">{dollar}{old_price}</p>
-                            <p className="price newPrice">{dollar}{newPrice}</p>
+                            <p className="item_price">{currency}{item_price}</p>
                         </div>
-                        <div className="cart">
-                            <svg className="outCart" xmlns="<http://www.w3.org/2000/svg>" viewBox="0 0 64 64">
-                                <path d="M2 6h10l10 40h32l8-24H16"></path>
-                                <circle cx="23" cy="54" r="4"></circle>
-                                <circle cx="49" cy="54" r="4"></circle>
-                            </svg>
-                        </div>
+                        <button className="cart">
+                            <MdOutlineShoppingCart />
+                        </button>
                     </div>
                 </div>
             </div>
--- src/components/cardSlider/CardSlider.jsx
+++ src/components/cardSlider/CardSlider.jsx
@@ -4,7 +4,7 @@ import food1 from '../../assets/foods/bruschetta.png';
 import food2 from '../../assets/foods/greeksalad.png';
 import food3 from '../../assets/foods/lemondesert.png';
 import blank from '../../assets/foods/blank.png';
-
+import cardData from './CardData';
 // Import Swiper components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from 'swiper/modules';
@@ -30,74 +30,20 @@ const CardSlider = () => {
                 1440: { slidesPerView: 3 }, // Large screens (3 slides)
                 1920: { slidesPerView: 4 }, // Large screens (3 slides)
             }}
-            className="mySwiper"
+            className="card-slider"
         >
-            <SwiperSlide>
-                <Card
-                    title="What is Lorem Ipsum?"
-                    images={food1}
-                    old_price="9,999"
-                    newPrice="9999"
-                    dollar="$"
-                    alt="bruschetta"
-                    exp_date="10-08-2022"
-                />
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card
-                    title="What is Lorem Ipsum?"
-                    images={food2}
-                    old_price="599"
-                    newPrice="500"
-                    dollar="$"
-                    alt="greeksalad"
-                    exp_date="10-08-2022"
-                />
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card
-                    title="What is Lorem Ipsum?"
-                    images={food3}
-                    old_price="7999"
-                    newPrice="7000"
-                    dollar="$"
-                    alt="lemondesert"
-                    exp_date="10-08-2022"
-                />
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card
-                    title="What is Lorem Ipsum?"
-                    images={blank}
-                    old_price="999"
-                    newPrice="500"
-                    dollar="$"
-                    alt="blank"
-                    exp_date="10-08-2022"
-                />
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card
-                    title="What is Lorem Ipsum?"
-                    images={blank}
-                    old_price="999"
-                    newPrice="500"
-                    dollar="$"
-                    alt="blank"
-                    exp_date="10-08-2022"
-                />
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card
-                    title="What is Lorem Ipsum?"
-                    images={blank}
-                    old_price="999"
-                    newPrice="500"
-                    dollar="$"
-                    alt="blank"
-                    exp_date="10-08-2022"
-                />
-            </SwiperSlide>
+            {cardData.map(({img, title, content, price}, index) => (
+                <SwiperSlide key={index}>
+                    <Card
+                        title={title}
+                        images={img}
+                        item_price={price}
+                        dollar=""
+                        alt={title}
+                    />
+                </SwiperSlide>
+            ))}
+            
         </Swiper>
     );
 };
